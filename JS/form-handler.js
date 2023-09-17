4987897ecbdfabb2b08411c47f8904adb8ab1704
Lirys <iamyourmarkieboo@gmail.com>
2023-09-17 12:41:38 +0800

    Added functionality

--- JS/form-handler.js
+++ JS/form-handler.js
@@ -3,8 +3,13 @@ form.addEventListener("submit", onSubmit);
 function onSubmit(e){
     e.preventDefault();
     let choice = document.querySelector("#selection").value;
+    let num1 = document.querySelector("#input1").value;
+    let num2 = document.querySelector("#input2").value;
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "function.php", true);
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send("choice="+choice);
+    xhr.onload = function () {
+        console.log(xhr.responseText);
+    }
+    xhr.send("choice="+choice+"&num1="+num1+"&num2="+num2);
 }
--- function.php
+++ function.php
@@ -1,3 +1,40 @@
 <?php
 
- echo $_POST["choice"];
+echo switchoo($_POST["choice"], $_POST["num1"], $_POST["num2"]);
+
+function switchoo($choice, $x, $y): int|float|string{
+    switch ($choice){
+        case "Addition":
+            return add($x, $y);
+        case "Subtraction":
+            return sub($x, $y);
+        case "Multiplication":
+            return mul($x, $y);
+        case "Division":
+            return div($x, $y);
+        case "Modulus":
+            return mod($x, $y);
+        default:
+            return "INVALID";
+    }
+}
+
+function add($x, $y){
+    return $x + $y;
+}
+
+function sub($x, $y){
+    return $x - $y;
+}
+
+function mul($x, $y){
+    return $x * $y;
+}
+
+function div($x, $y){
+    return $x / $y;
+}
+
+function mod($x, $y){
+    return $x % $y;
+}
